d8d70cc41d2c9163edee76ac7a9c5d287049cec8
AlanSZK <54686091+AlanSZK@users.noreply.github.com>
2022-07-18 16:42:31 -0400

    Areglado registro y enlazado con firestore

--- app/src/main/java/com/example/primerproyectogrupalcompmovil/SignupActivity.java
+++ app/src/main/java/com/example/primerproyectogrupalcompmovil/SignupActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -32,8 +33,7 @@ public class SignupActivity extends AppCompatActivity {
     EditText etUsuario;
     String uuidUsuario;
 
-    boolean exitoAuth = false;
-    boolean exitoFirestore = false;
+    boolean exito=false;
     private String TAG;
 
 
@@ -52,88 +52,64 @@ public class SignupActivity extends AppCompatActivity {
     public void registrar(View view) {
 
 
-
         if ((!etEmailSignup.getText().toString().isEmpty() && !etEmailContrasenaSignup.getText().toString().isEmpty() && !etUsuario.getText().toString().isEmpty())) {
-
-            registroAutenticacion();
-            registroFirestore();
-
-            if (exitoAuth==true && exitoFirestore==true) {
-                Toast.makeText(getApplicationContext(), "Registro realizado con éxito!", Toast.LENGTH_SHORT).show();
-                exitoAuth=false;
-                exitoFirestore=false;
-                finish();
-
-            }
-
-        } else {
-            Toast.makeText(getApplicationContext(), "Datos incorrectos", Toast.LENGTH_SHORT).show();
-        }
-
-
-        }
-
-
-        public void registroAutenticacion()
-        {
 
             //Registro de correo y contraseña en Firebase
             auth = FirebaseAuth.getInstance();
 
+            auth.getInstance().createUserWithEmailAndPassword(etEmailSignup.getText().toString(), etEmailContrasenaSignup.getText().toString()).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
 
-            Task<AuthResult> task = auth.getInstance().createUserWithEmailAndPassword(etEmailSignup.getText().toString(), etEmailContrasenaSignup.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+
                     if (task.isSuccessful()) {
 
-                        uuidUsuario=task.getResult().getUser().getUid();
-                        exitoAuth = true;
+                        //Registro de usuario en base de datos firestore
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+                        Map<String, Object> usuario = new HashMap<>();
+                        usuario.put("authUuid",auth.getUid().toString());
+                        usuario.put("email", etEmailSignup.getText().toString());
+                        usuario.put("nombre", etUsuario.getText().toString());
+
+
+
+                        db.collection("usuario")
+                                .add(usuario)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                                        Toast.makeText(getApplicationContext(), "Registro realizado con éxito!", Toast.LENGTH_SHORT).show();
+                                        finish();
+
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w("TAG", "Error adding document", e);
+                                        Toast.makeText(getApplicationContext(), "Datos incorrectos", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
 
                     } else {
                         Toast.makeText(getApplicationContext(), "Datos incorrectos", Toast.LENGTH_SHORT).show();
                     }
                 }
-
             });
-
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Datos incorrectos", Toast.LENGTH_SHORT).show();
         }
 
-        public void registroFirestore() {
-            //Registro de usuario en base de datos firestore
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-            Map<String, Object> usuario = new HashMap<>();
-            usuario.put("authUuid",uuidUsuario);
-            usuario.put("nombre",etUsuario.getText().toString());
-
-            db.collection("usuario")
-                    .add(usuario)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
-                            exitoFirestore=true;
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w("TAG", "Error adding document", e);
-                        }
-                    });
-
-
-
-
-        }
-
+    }
 }
+
+
+
 
 
 
