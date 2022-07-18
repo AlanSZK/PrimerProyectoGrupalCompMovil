0bf1f38e8ba3446ae8fe090eb535838bc8cc08bc
AlanSZK <54686091+AlanSZK@users.noreply.github.com>
2022-07-18 14:55:35 -0400

    Añadida conexión con firestore

--- app/src/main/java/com/example/primerproyectogrupalcompmovil/SignupActivity.java
+++ app/src/main/java/com/example/primerproyectogrupalcompmovil/SignupActivity.java
@@ -4,15 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -20,6 +29,12 @@ public class SignupActivity extends AppCompatActivity {
 
     EditText etEmailSignup;
     EditText etEmailContrasenaSignup;
+    EditText etUsuario;
+    String uuidUsuario;
+
+    boolean exitoAuth = false;
+    boolean exitoFirestore = false;
+    private String TAG;
 
 
     @Override
@@ -29,34 +44,95 @@ public class SignupActivity extends AppCompatActivity {
 
         etEmailSignup = findViewById(R.id.etEmailSignup);
         etEmailContrasenaSignup = findViewById((R.id.etContrasenaSignup));
+        etUsuario = findViewById(R.id.etUsuario);
 
         setTitle("Registrarse");
     }
 
     public void registrar(View view) {
-        if (!etEmailSignup.getText().toString().isEmpty() && !etEmailContrasenaSignup.getText().toString().isEmpty()) {
+
+
+
+        if ((!etEmailSignup.getText().toString().isEmpty() && !etEmailContrasenaSignup.getText().toString().isEmpty() && !etUsuario.getText().toString().isEmpty())) {
+
+            registroAutenticacion();
+            registroFirestore();
+
+            if (exitoAuth==true && exitoFirestore==true) {
+                Toast.makeText(getApplicationContext(), "Registro realizado con éxito!", Toast.LENGTH_SHORT).show();
+                exitoAuth=false;
+                exitoFirestore=false;
+                finish();
+
+            }
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Datos incorrectos", Toast.LENGTH_SHORT).show();
+        }
+
+
+        }
+
+
+        public void registroAutenticacion()
+        {
+
+            //Registro de correo y contraseña en Firebase
             auth = FirebaseAuth.getInstance();
 
-            auth.getInstance().createUserWithEmailAndPassword(etEmailSignup.getText().toString(), etEmailContrasenaSignup.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            Task<AuthResult> task = auth.getInstance().createUserWithEmailAndPassword(etEmailSignup.getText().toString(), etEmailContrasenaSignup.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(getApplicationContext(), "Registro realizado con éxito!", Toast.LENGTH_SHORT).show();
-                        finish();
-
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                    else{
-                        Toast.makeText(getApplicationContext(), "Email o Contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                    if (task.isSuccessful()) {
+
+                        uuidUsuario=task.getResult().getUser().getUid();
+                        exitoAuth = true;
+
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Datos incorrectos", Toast.LENGTH_SHORT).show();
                     }
                 }
+
             });
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "Email o Contraseña incorrectos", Toast.LENGTH_SHORT).show();
+
         }
 
+        public void registroFirestore() {
+            //Registro de usuario en base de datos firestore
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    }
+            Map<String, Object> usuario = new HashMap<>();
+            usuario.put("authUuid",uuidUsuario);
+            usuario.put("nombre",etUsuario.getText().toString());
+
+            db.collection("usuario")
+                    .add(usuario)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                            exitoFirestore=true;
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("TAG", "Error adding document", e);
+                        }
+                    });
+
+
+
+
+        }
+
 }
 
 
