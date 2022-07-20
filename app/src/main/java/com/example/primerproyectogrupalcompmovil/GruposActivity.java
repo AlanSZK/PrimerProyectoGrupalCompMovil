e328a064c106ebcc9daedd6dbdef906a68e1cd38
Scalim <javier.garridor@utem.cl>
2022-07-20 02:24:47 -0400

    upd8

--- app/src/main/java/com/example/primerproyectogrupalcompmovil/GruposActivity.java
+++ app/src/main/java/com/example/primerproyectogrupalcompmovil/GruposActivity.java
@@ -2,6 +2,7 @@ package com.example.primerproyectogrupalcompmovil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +11,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class GruposActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
 
 
@@ -21,6 +27,31 @@ public class GruposActivity extends AppCompatActivity {
         setContentView(R.layout.activity_grupos);
 
         setTitle("Grupos");
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null)
+        {
+            SendUserToLoginActivity();
+        }
+    }
+
+    private void SendUserToLoginActivity()
+    {
+
+        Intent loginIntent = new Intent(GruposActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
 
     }
 
